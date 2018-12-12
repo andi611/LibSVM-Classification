ada99fa9388971f82d0597e4bfe4420be3539ae0
andi611 <tingweiandyliu@gmail.com>
2018-12-12 17:34:37 +0800

    update

--- src/1_iris.sh
+++ src/1_iris.sh
@@ -11,5 +11,6 @@ TRAIN_DATA_PATH=${2:-../data/iris/iris.tr}
 TEST_DATA_PATH=${3:-../data/iris/iris.te}
 OUTPUT_FILE_PATH=${4:-../result/iris_predict.csv}
 
+echo "Running different kernal functions..."
 $LIBSVM_PATH/svm-train $TRAIN_DATA_PATH ./model_iris.libsvm
-$LIBSVM_PATH/svm-test $TEST_DATA_PATH ./model_iris.libsvm $OUTPUT_FILE_PATH
+$LIBSVM_PATH/svm-predict $TEST_DATA_PATH ./model_iris.libsvm $OUTPUT_FILE_PATH
