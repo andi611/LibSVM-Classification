684dca6e19c054f3171012a79deb198cb31bbf70
andi611 <tingweiandyliu@gmail.com>
2018-12-12 20:53:12 +0800

    update

--- src/2_news.sh
+++ src/2_news.sh
@@ -9,14 +9,14 @@
 
 #---arguments---#
 LIBSVM_PATH=${1:-/home/andi611/LibSVM-Classification/libsvm-3.23}
-TRAIN_DATA_PATH=${2:-../data/iris/iris.tr}
-TEST_DATA_PATH=${3:-../data/iris/iris.te}
-OUTPUT_FILE_PATH=${4:-../result/iris_predict.csv}
+TRAIN_DATA_PATH=${2:-../data/news/news.tr}
+TEST_DATA_PATH=${3:-../data/news/news.te}
+OUTPUT_FILE_PATH=${4:-../result/news_predict.csv}
 
 
 #---variables---#
-MODEL_NAME=model_iris.libsvm
-RUN_BEST=True
+MODEL_NAME=model_news.libsvm
+RUN_BEST=Trues
 
 
 #---run best---#
@@ -26,7 +26,11 @@ if [ "${RUN_BEST}" = True ] ; then
 	echo "|------ Running with Best Parameters ------|"
 	echo "|------------------------------------------|"
 
-	$LIBSVM_PATH/svm-train -s 0 -t 0 ${TRAIN_DATA_PATH} ${MODEL_NAME}
+	$LIBSVM_PATH/svm-train -s 0 -t 0 -q ${TRAIN_DATA_PATH} ${MODEL_NAME}
+	echo "Training:"
+	$LIBSVM_PATH/svm-predict ${TRAIN_DATA_PATH} ${MODEL_NAME} ${OUTPUT_FILE_PATH}.train
+	rm ${OUTPUT_FILE_PATH}.train
+	echo "Testing:"
 	$LIBSVM_PATH/svm-predict ${TEST_DATA_PATH} ${MODEL_NAME} ${OUTPUT_FILE_PATH}
 
 #---search best---#
@@ -55,8 +59,8 @@ else
 	do
 		echo
 		echo ">>> Kernal function: ${kernals[idx]}"
-		$LIBSVM_PATH/svm-scale -l -1 -u 1 ${TRAIN_DATA_PATH} > ${TRAIN_DATA_PATH}.scale
-		$LIBSVM_PATH/svm-scale -l -1 -u 1 ${TEST_DATA_PATH} > ${TEST_DATA_PATH}.scale
+		$LIBSVM_PATH/svm-scale -l 0 -u 10 ${TRAIN_DATA_PATH} > ${TRAIN_DATA_PATH}.scale
+		$LIBSVM_PATH/svm-scale -l 0 -u 10 ${TEST_DATA_PATH} > ${TEST_DATA_PATH}.scale
 		$LIBSVM_PATH/svm-train -s 0 -t ${idx} -q ${TRAIN_DATA_PATH}.scale ${MODEL_NAME}.temp
 		$LIBSVM_PATH/svm-predict ${TEST_DATA_PATH}.scale ${MODEL_NAME}.temp ${OUTPUT_FILE_PATH}
 		rm ${TRAIN_DATA_PATH}.scale ${TEST_DATA_PATH}.scale ${OUTPUT_FILE_PATH} ${MODEL_NAME}.temp
