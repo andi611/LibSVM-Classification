a3d2200355bf03e9553db502278ee193ba84095c
andi611 <tingweiandyliu@gmail.com>
2018-12-12 16:57:35 +0800

    update

--- src/1_iris.sh
+++ src/1_iris.sh
@@ -6,5 +6,10 @@
 #   Copyright    [ Copyleft(c), NTUEE, NTU, Taiwan ]
 #*********************************************************************************************#
 
-LIBSVM_PATH ?= /home/andi611/dm/libsvm-3.23
-./$(LIBSVM_PATH)/svm-train
+LIBSVM_PATH=${1:-/home/andi611/dm/libsvm-3.23}
+TRAIN_DATA_PATH=${2:-../data/iris/iris.tr}
+TEST_DATA_PATH=${3:-../data/iris/iris.te}
+OUTPUT_FILE_PATH=${4:-../result/iris_predict.csv}
+
+./$(LIBSVM_PATH)/svm-train $(TRAIN_DATA_PATH) ./model_iris.libsvm
+./$(LIBSVM_PATH)/svm-test $(TEST_DATA_PATH) ./model_iris.libsvm $(OUTPUT_FILE_PATH)
