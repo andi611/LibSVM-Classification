86c7312453c15a6c6e0f5ecbce5e72660e941edc
andi611 <tingweiandyliu@gmail.com>
2018-12-13 13:00:24 +0800

    update

--- src/2_news.sh
+++ src/2_news.sh
@@ -17,7 +17,8 @@ OUTPUT_FILE_PATH=${4:-../result/news_predict.csv}
 #---variables---#
 MODEL_NAME=model_news.libsvm
 #MODE=RUN_BEST
-MODE=COMPARE_KERNAL
+#MODE=COMPARE_KERNAL
+#MODE=COMPARE_NUSVC
 #MODE=COMPARE_SCALE
 #MODE=RUN_ALL
 
@@ -53,6 +54,32 @@ if [ "${MODE}" = COMPARE_KERNAL ] || [ "${MODE}" = RUN_ALL ] ; then
 		rm ${OUTPUT_FILE_PATH} ${MODEL_NAME}.temp
 	done
 fi
+if [ "${MODE}" = COMPARE_NUSVM ] || [ "${MODE}" = RUN_ALL ] ; then
+	echo
+	echo "|--------------------------------------------------------|"
+	echo "|------ Comparing Different Linear NU-SVM Settings ------|"
+	echo "|--------------------------------------------------------|"
+
+	nus=( 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 )
+	for ((idx=0; idx<${#nus[@]}; ++idx));
+	do
+		echo
+		echo ">>> nu value: ${nus[idx]}"
+		$LIBSVM_PATH/svm-train -s 1 -t 0 -n ${nus[idx]} -q ${TRAIN_DATA_PATH} ${MODEL_NAME}.temp
+		$LIBSVM_PATH/svm-predict ${TEST_DATA_PATH} ${MODEL_NAME}.temp ${OUTPUT_FILE_PATH}
+		rm ${OUTPUT_FILE_PATH} ${MODEL_NAME}.temp
+	done
+
+	epsilons=( 0.0001 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.01 )
+	for ((idx=0; idx<${#epsilons[@]}; ++idx));
+	do
+		echo
+		echo ">>> epsilon value: ${epsilons[idx]}"
+		$LIBSVM_PATH/svm-train -s 1 -t 0 -e ${epsilons[idx]} -q ${TRAIN_DATA_PATH} ${MODEL_NAME}.temp
+		$LIBSVM_PATH/svm-predict ${TEST_DATA_PATH} ${MODEL_NAME}.temp ${OUTPUT_FILE_PATH}
+		rm ${OUTPUT_FILE_PATH} ${MODEL_NAME}.temp
+	done
+fi
 if [ "${MODE}" = COMPARE_SCALE ] || [ "${MODE}" = RUN_ALL ] ; then
 	echo
 	echo "|--------------------------------------------------------------|"
@@ -65,7 +92,7 @@ if [ "${MODE}" = COMPARE_SCALE ] || [ "${MODE}" = RUN_ALL ] ; then
 		echo ">>> Kernal function: ${kernals[idx]}"
 		$LIBSVM_PATH/svm-scale -l 0 -u 10 ${TRAIN_DATA_PATH} > ${TRAIN_DATA_PATH}.scale
 		$LIBSVM_PATH/svm-scale -l 0 -u 10 ${TEST_DATA_PATH} > ${TEST_DATA_PATH}.scale
-		$LIBSVM_PATH/svm-train -s 0 -t ${idx} -q ${TRAIN_DATA_PATH}.scale ${MODEL_NAME}.temp
+		$LIBSVM_PATH/svm-train -s 1 -t ${idx} -q ${TRAIN_DATA_PATH}.scale ${MODEL_NAME}.temp
 		$LIBSVM_PATH/svm-predict ${TEST_DATA_PATH}.scale ${MODEL_NAME}.temp ${OUTPUT_FILE_PATH}
 		rm ${TRAIN_DATA_PATH}.scale ${TEST_DATA_PATH}.scale ${OUTPUT_FILE_PATH} ${MODEL_NAME}.temp
 	done
